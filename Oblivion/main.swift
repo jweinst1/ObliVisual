37d0623631ca4d4591adbc8bb1b1f85603166527
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-02-24 14:38:11 -0800

    first commit

---
+++ Oblivion/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Oblivion
+//
+//  Created by Joshua Weinstein on 2/24/16.
+//  Copyright (c) 2016 Joshua Weinstein. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, World!")
+print("The Oblivion Programming Lanuage")
+print("Version 1.0.0")
+
