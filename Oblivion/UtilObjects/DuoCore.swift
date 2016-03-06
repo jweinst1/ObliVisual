19065114edf08d9e13a36befc4881ed5258d066c
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-06 12:21:38 -0800

    duocore

--- Oblivion/UtilObjects/DuoCore.swift
+++ Oblivion/UtilObjects/DuoCore.swift
@@ -7,24 +7,26 @@ import Foundation
 
 //struct that supports operations done on two elements at a time.
 struct DuoCore<elem> {
-    var previous:elem?
+    var previous:[elem]
     var next:elem?
     //allows core to be initialized with no values
     init() {
-
+        self.previous = [elem]()
     }
     //insertion method with priority and only two space memory
     mutating func insert(member:elem) {
-        if self.previous == nil {
-            self.previous = member
-        }
-        else if self.next == nil {
+        if self.next == nil {
             self.next = member
         }
         else {
-            let holder = self.next
-            self.previous = holder
+            self.previous.insert(self.next!, atIndex: 0)
             self.next = member
         }
     }
+    //fuses the two current elements into an array on the previous node
+
+    mutating func setnext(member:elem) {
+        self.next = member
+    }
+
 }
