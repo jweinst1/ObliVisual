6768153dddc3e37e194ebb37878e048247ee4e1f
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-09 14:58:34 -0800

    experimental token matching

---
+++ Experimental/TokenExtraction.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//playground for testing token extraction
+
+
+//: Playground - noun: a place where people can play
+extension String {
+    func matchPattern(patStr:String)->Bool {
+        var isMatch:Bool = false
+        do {
+            let regex = try NSRegularExpression(pattern: patStr, options: [.CaseInsensitive])
+            let result = regex.firstMatchInString(self, options: NSMatchingOptions(rawValue: 0), range: NSMakeRange(0, characters.count))
+            
+            if (result != nil)
+            {
+                isMatch = true
+            }
+        }
+        catch {
+            isMatch = false
+        }
+        return isMatch
+    }
+}
+
+
+//extension that allows for matches in a string to be extracted
+extension String {
+    func matchesForRegexInText(regex: String!) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let nsString = self as NSString
+            let results = regex.matchesInString(self,
+                options: [], range: NSMakeRange(0, nsString.length))
+            return results.map { nsString.substringWithRange($0.range)}
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
+
+func ExtractTokens(line:String) -> [String] {
+    return line.matchesForRegexInText("[0-9]+|\\+>|\\+")
+}
+
+ExtractTokens("5+6 +> 7")
