8878b7356ac0d1e7289d912133dde4311a203ff8
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-09 00:02:13 -0800

    update to assembler

--- Experimental/TokenAssembler.playground/Contents.swift
+++ Experimental/TokenAssembler.playground/Contents.swift
@@ -59,8 +59,8 @@ struct Token {
     }
 }
 
-var sample = "5 + 6 - 8 * 4"
-let mathpat = "\\+|\\-|\\*|[0-9]+"
+var sample = "5&7^6+1"
+let mathpat = "\\+|\\-|\\*|[0-9]+|\\^|_|\\&"
 
 
 //sample tokenizer
@@ -73,6 +73,28 @@ struct Tokenizer {
         return newtokens
     }
 }
+
+struct operfuncs {
+    static func max(num1:Int, num2:Int) -> Int {
+        if num1 > num2 {
+            return num1
+        }
+        else {
+            return num2
+        }
+    }
+    
+    static func min(num1:Int, num2:Int) -> Int {
+        if num1 < num2 {
+            return num1
+        }
+        else {
+            return num2
+        }
+    }
+}
+
+
 // sample tokens made
 var sampletokens = Tokenizer.tokenize(sample.matchesForRegexInText(mathpat))
 
@@ -83,8 +105,9 @@ struct Lexer {
             let template = list[i].element as! String
             if template.matchPattern("[1-9][0-9]*") {
                 list[i].settype("int")
+                list[i].element = Int(template)!
             }
-            else if template.matchPattern("\\+|\\*|\\-") {
+            else if template.matchPattern("\\+|\\*|\\-|\\^|_|\\&") {
                 list[i].settype("oper")
             }
         }
@@ -94,6 +117,46 @@ struct Lexer {
 //tokens are lexed
 var lexedtokens = Lexer.lextokens(sampletokens)
 
+//assembler which carries two distinct types of assemble styles
 struct Assembler {
     
+    static func linearassemble(instructions:[Token]) -> Void {
+        //default initialization
+        var current = 0
+        for(var i=0;i<instructions.count;i++) {
+            if instructions[i].type == "int" && i == 0 {
+                current = instructions[i].element as! Int
+            }
+            else if instructions[i].type == "oper" {
+                let temp = instructions[i].element as! String
+                switch(temp) {
+                case "+":
+                    current += instructions[i+1].element as! Int
+                case "^":
+                    current = operfuncs.max(current, num2: instructions[i+1].element as! Int)
+                case "_":
+                    current = operfuncs.min(current, num2: instructions[i+1].element as! Int)
+                case "-":
+                    current -= instructions[i+1].element as! Int
+                case "&":
+                    current = Int(String(current) + String(instructions[i+1].element as! Int))!
+                case "*":
+                    current *= instructions[i+1].element as! Int
+                default:
+                    print("OPER ERROR")
+                }
+            }
+        }
+        print(current)
+    }
+}
+// assembles the tokens to a result
+Assembler.linearassemble(lexedtokens)
+
+//top level function to process input lines for user
+func Interpret(line:String) -> Void {
+    let sample = line.matchesForRegexInText(mathpat)
+    let tokens = Tokenizer.tokenize(sample)
+    let lexed = Lexer.lextokens(tokens)
+    Assembler.linearassemble(lexed)
 }
