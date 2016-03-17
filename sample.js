e0e69ed5a3e9b68a0b9b17296dccc19da66f1549
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-16 23:26:59 -0700

    json system

--- sample.js
+++ sample.js
@@ -1,7 +1,20 @@
-/**
- * Created by Josh on 3/16/16.
- */
+#!/usr/bin/env node
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
 
-//hello
+rl.setPrompt('TTT> ');
+rl.prompt();
 
-console.log("hello");
+rl.on('line', function(line) {
+    switch(line.trim()) {
+        case 'hello':
+            console.log('world!');
+            break;
+        case 'close':
+            process.exit(0);
+        default:
+            console.log('Say what? I might have heard `' + line.trim() + '`');
+            break;
+    }
+    rl.prompt();
+});
