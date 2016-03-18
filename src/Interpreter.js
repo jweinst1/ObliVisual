fc61863581dc4c4fdcd217aababa6a5472a71d0f
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-18 12:30:58 -0700

    interpreter working

--- src/Interpreter.js
+++ src/Interpreter.js
@@ -6,17 +6,35 @@ var Interpreter = (function () {
 
     Interpreter.interpretLine = function (line) {
         var tokens = line.split(" ");
+        var arguments = [];
         var current = cmds;
+        console.log(current);
         while (tokens.length>0) {
-            var temp = tokens.shift()
+            var temp = tokens.shift();
             if (temp in current) {
-                current = current[temp]
+                current = current[temp];
+                console.log(current)
+            }
+            else if ("**arg**" in current) {
+                arguments.push(temp);
+                current = current["**arg**"];
+                console.log(current)
             }
             else {
                 //breaks loop if non-formed statement encountered
                 return "Invalid Token or Statement"
             }
         }
+        if ("[addition]" in current) {
+            return parseInt(arguments[0]) + parseInt(arguments[1])
+        }
+        else {
+            return "Err"
+        }
     };
     return Interpreter;
 })();
+
+
+var test = "add 6 to 7";
+console.log(Interpreter.interpretLine(test));
