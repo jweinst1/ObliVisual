ade5a81cee36e71f26007f1549fa0d278781bf9f
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-18 17:46:06 -0700

    math functions

--- src/Assembler.js
+++ src/Assembler.js
@@ -2,14 +2,46 @@
  * Created by Josh on 3/18/16.
  */
 
-    //assembles math statements
+    //assembles math statements from arrays
 var MathAssembler = {
     add:function(numbers) {
-        var total = 0
+        var total = 0;
         for (elem in numbers) {
-            total += numbers[elem]
+            total += numbers[elem];
         }
-        return total
+        return total;
+    },
+    subtract:function(numbers) {
+        var total = numbers.shift();
+        for (elem in numbers) {
+            total -= numbers[elem];
+        }
+        return total;
+    },
+    multiply:function(numbers) {
+        var total = 1;
+        for (elem in numbers) {
+            total *= numbers[elem];
+        }
+        return total;
+    },
+    divide:function(numbers) {
+        var total = 1;
+        for (elem in numbers) {
+            if (numbers[elem] == 0) {
+                total /= 1
+            }
+            total /= numbers[elem];
+        }
+        return total;
     }
+
+};
+
+exports.MathAssembler = MathAssembler;
+
+//static functions that deal with 1 or 2 arguments, non-array
+var MathFunctions = {
+
 };
 
