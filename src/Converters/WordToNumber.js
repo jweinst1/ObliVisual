ea3d3b76539730c21ac1338cf52413b6e74c4769
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-19 11:53:17 -0700

    conversions

--- src/Converters/WordToNumber.js
+++ src/Converters/WordToNumber.js
@@ -3,3 +3,16 @@
  */
 var nums = require("./numberphrases.json");
 
+var toNumber = function(phrase) {
+    var words = phrase.split(" ");
+    var number = 0;
+    for(var i=0;i<words.length;i++) {
+        switch(words[i]) {
+            case "one":
+                number += 1;
+                break;
+            default:
+                continue
+        }
+    }
+};
