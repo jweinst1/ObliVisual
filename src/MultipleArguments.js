a1521b55e7a28d1ec57dd83b7ba9fce655dfc706
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-24 00:32:33 -0700

    new var dict.

--- src/MultipleArguments.js
+++
@@ -1,8 +0,0 @@
-//javascript file for handling multiple arguments
-
-var extractargs = function(string) {
-    var newstring = string.slice(1, string.length-1);
-    return newstring.split(",");
-};
-
-exports.extractargs = extractargs;
---
+++ src/VariableDictionary.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Josh on 3/24/16.
+ */
+
+//functions as overloaded object wrapper to store variables and objects.
+var VariableDict = function() {
+    VariableDict.prototype.set = function(name, obj) {
+        this[name] = obj;
+    };
+    VariableDict.prototype.get = function(name) {
+        return this[name];
+    };
+    VariableDict.prototype.check = function(name) {
+        return name in this;
+    };
+};
+
+exports.VariableDict = VariableDict;
