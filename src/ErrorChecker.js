1b06d85a94518c98ea9454b4980727fcb5999864
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-03-26 19:09:13 -0700

    error checker

--- src/ErrorChecker.js
+++ src/ErrorChecker.js
@@ -5,7 +5,26 @@
 
 var checkargs = function(statement, args) {
     switch(statement) {
+        case "[addition]":
+            return checkfornumbers(args);
+            break;
+        case "[subtraction]":
+            return checkfornumbers(args);
+            break;
         default:
             for(var key in args) if(agrs[key].constructor===ErrorObj) return true;
     }
+};
+
+//exports the function that uses all other error functions
+exports.checkargs = checkargs;
+
+//checks number arguments
+var checkfornumbers = function(args) {
+    for(var key in args) {
+        if(args[key].constructor !== NumberObj) {
+            return true;
+        }
+    }
+    return false;
 };
