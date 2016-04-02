0cf5debc6b5fb538e73c838e23006722a96c7022
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-04-01 19:13:34 -0700

    ts implementation of objects

---
+++ src/builtinobjects/PrimitiveObjects.js
@@ -0,0 +1,9 @@
+//typescript version
+var Obj = (function () {
+    function Obj(value, type) {
+        this.value = value;
+        this.type = type;
+    }
+    return Obj;
+})();
+//# sourceMappingURL=PrimitiveObjects.js.map
---
+++ src/builtinobjects/PrimitiveObjects.ts
@@ -0,0 +1,11 @@
+//typescript version
+
+class Obj {
+    public value:any;
+    public type:string;
+
+    constructor(value:any, type:string) {
+        this.value = value;
+        this.type = type;
+    }
+}
