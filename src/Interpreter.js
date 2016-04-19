7b8eedf96ac6c669ee336b784a0f152a9526cc5e
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-04-19 12:40:56 -0700

    interpreter

--- src/Interpreter.js
+++ src/Interpreter.js
@@ -12,5 +12,27 @@ var Interpreter = (function(){
         this.global = new vd.VariableDict();
         this.current = null;
     }
+    Interpreter.prototype.splitcode = function(input){
+        var slices = input.split(/(\[.*?\])/);
+        var tokens = [];
+        for(var i=0;i<slices.length;i++) {
+            if(/\[.*?\]/.test(slices[i])) {
+                tokens.push(slices[i]);
+            }
+        }
+        return tokens;
+    };
+    //the interpreter works by processing a single unit at a time, rather than lines of code
+    Interpreter.prototype.processUnit = function(input) {
+
+    };
+    //this method is used to process multiple units at a time, or documents of code.
+    Interpreter.prototype.processCode = function(code) {
+        var tokens = this.splitcode(code);
+        for(var i=0;i<tokens.length;i++) this.processUnit(tokens[i]);
+    };
     return Interpreter;
 })();
+
+
+
